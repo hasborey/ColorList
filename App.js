7d50c7bd05dd3fe78f34da34103b1bc3533cbb1d
borey <borey@web-essentials.asia>
2018-03-09 22:11:14 +0700

    Add ScrollView

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {
   Text,
-  View,
+  ScrollView,
   StyleSheet,
   TouchableHighlight,
 } from 'react-native'
@@ -25,9 +25,36 @@ export default class App extends Component {
     const { backgroundColor } = this.state
 
     return (
-      <View style={[styles.container, { backgroundColor }]}>
-        <ColorButton backgroundColor="green" onSelect={this.changeColor} />
-      </View>
+      <ScrollView style={[styles.container, { backgroundColor }]}>
+        <ColorButton backgroundColor="khaki" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="lavender" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="lavenderblush" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="lightblue" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="aliceblue" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="antiquewhite" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="aqua" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="aquamarine" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="azure" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="beige" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="bisque" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="brown" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="burlywood" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="cadetblue" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="chartreuse" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="chocolate" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="coral" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="cornflowerblue" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="cornsilk" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="crimson" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="cyan" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="darkblue" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="darkcyan" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="darkgoldenrod" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="darkgray" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="darkgreen" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="darkkhaki" onSelect={this.changeColor} />
+        <ColorButton backgroundColor="darkmagenta" onSelect={this.changeColor} />
+      </ScrollView>
     )
   }
 }
@@ -35,8 +62,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 30,
   },
 })
