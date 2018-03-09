a247a03a4a53541f9ba13f6db4ae89c012fc1573
borey <borey@web-essentials.asia>
2018-03-09 21:53:45 +0700

    Add sample color and underlayColor

--- App.js
+++ App.js
@@ -25,15 +25,23 @@ export default class App extends Component {
       <View style={[styles.container, { backgroundColor }]}>
         <TouchableHighlight
           onPress={() => this.setBgColor('lightgreen')}
+          underlayColor='gray'
           style={styles.button}
         >
-          <Text>Green</Text>
+          <View style={styles.row}>
+            <View style={[styles.sample, {backgroundColor: 'lightgreen'}]} />
+            <Text style={styles.text}>Green</Text>
+          </View>
         </TouchableHighlight>
         <TouchableHighlight
           onPress={() => this.setBgColor('lightcoral')}
+          underlayColor='gray'
           style={styles.button}
         >
-          <Text>Red</Text>
+          <View style={styles.row}>
+            <View style={[styles.sample, {backgroundColor: 'lightcoral'}]} />
+            <Text style={styles.text}>Red</Text>
+          </View>
         </TouchableHighlight>
       </View>
     )
@@ -48,12 +56,25 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   button: {
-    borderWidth: 2,
+    borderWidth: 1,
     borderRadius: 10,
     padding: 10,
     margin: 10,
     alignSelf: 'stretch',
     textAlign: 'center',
     backgroundColor: 'rgba(255, 255, 255,.5)',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  text: {
+    fontSize: 18,
+  },
+  sample: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    marginRight: 10,
+    borderWidth: 1,
   }
 })
